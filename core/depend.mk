794938fe4c043d352e6cc8d3b8bf0b8c59822c4f
MNogFonseca <matha.nogueira@gmail.com>
2015-11-02 22:30:17 -0200

    first commit

---
+++ core/depend.mk
@@ -0,0 +1,27 @@
+/home/matheus/Downloads/minisat/core/Main.o /home/matheus/Downloads/minisat/core/Main.or /home/matheus/Downloads/minisat/core/Main.od /home/matheus/Downloads/minisat/core/Main.op: /home/matheus/Downloads/minisat/core/Main.cc \
+ /home/matheus/Dropbox/minisat/utils/System.h \
+ /home/matheus/Dropbox/minisat/mtl/IntTypes.h \
+ /home/matheus/Dropbox/minisat/utils/ParseUtils.h \
+ /home/matheus/Dropbox/minisat/utils/Options.h \
+ /home/matheus/Dropbox/minisat/mtl/Vec.h \
+ /home/matheus/Dropbox/minisat/mtl/XAlloc.h \
+ /home/matheus/Dropbox/minisat/core/Dimacs.h \
+ /home/matheus/Dropbox/minisat/core/SolverTypes.h \
+ /home/matheus/Dropbox/minisat/mtl/Alg.h \
+ /home/matheus/Dropbox/minisat/mtl/Map.h \
+ /home/matheus/Dropbox/minisat/mtl/Alloc.h \
+ /home/matheus/Dropbox/minisat/core/Solver.h \
+ /home/matheus/Dropbox/minisat/mtl/Heap.h
+/home/matheus/Downloads/minisat/core/Solver.o /home/matheus/Downloads/minisat/core/Solver.or /home/matheus/Downloads/minisat/core/Solver.od /home/matheus/Downloads/minisat/core/Solver.op: /home/matheus/Downloads/minisat/core/Solver.cc \
+ /home/matheus/Dropbox/minisat/mtl/Sort.h \
+ /home/matheus/Dropbox/minisat/mtl/Vec.h \
+ /home/matheus/Dropbox/minisat/mtl/IntTypes.h \
+ /home/matheus/Dropbox/minisat/mtl/XAlloc.h \
+ /home/matheus/Dropbox/minisat/core/Solver.h \
+ /home/matheus/Dropbox/minisat/mtl/Heap.h \
+ /home/matheus/Dropbox/minisat/mtl/Alg.h \
+ /home/matheus/Dropbox/minisat/utils/Options.h \
+ /home/matheus/Dropbox/minisat/utils/ParseUtils.h \
+ /home/matheus/Dropbox/minisat/core/SolverTypes.h \
+ /home/matheus/Dropbox/minisat/mtl/Map.h \
+ /home/matheus/Dropbox/minisat/mtl/Alloc.h
